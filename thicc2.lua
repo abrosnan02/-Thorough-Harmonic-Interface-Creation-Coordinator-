aae152b382411ca935188319161c0a15a71f6cdd
Walnut <44645606+EWalnut@users.noreply.github.com>
2019-07-27 01:58:34 -0400

    Add files via upload
    Added buttons, fixed text wrapping, overhauled tweening

--- thicc2.lua
+++ thicc2.lua
@@ -3,26 +3,28 @@
     Anthony Brosnan, July 2019
 
     Independent library, optional tweening
-    TODO: tweens, postprocessing, shadows, dynamic text and other scaling, outlines, textwrap, rotation
+    Easing equasion module available here: https://github.com/EmmanuelOga/easing/blob/master/lib/easing.lua 
+    TODO: padding, postprocessing, shadows, dynamic text and other scaling, outlines, rotation, UDim2 single sets
 
-    Easing equasion module available here: https://github.com/EmmanuelOga/easing/blob/master/lib/easing.lua
+    This library uses the GPLv2 license, which can be read in the LICENSE.txt file.
 ]]--
 
 --/Main Variables/--------------------------------------------------------------
 local Thicc2 = {}
 local elements = {}
 local effects = {}
-local tweens = {}
 local tweenFunctions = {}
-local dt = 1/60
+local dt = 0 --for one frame at the start no tweening can be performed
 local easingModule = require('easing')
+local mousePrevDown = nil
+
 
 --/THICC Variables/-------------------------------------------------------------
 Thicc2.settings = { --unused atm
 }
 
 
---/UDim/------------------------------------------------------------------------
+--/UDim2/-----------------------------------------------------------------------
 local function udim2(element, xs, xp, ys, yp, size)
 	local maxWidth, maxHeight = love.graphics.getDimensions()
 
@@ -56,9 +58,27 @@ end
 
 
 --/Tweens/----------------------------------------------------------------------
+local function checkDupeTweens(element,type)
+    local tweens = {}
+    for k,v in pairs(element.tweens) do
+        if v.type == type then
+            table.insert(tweens,v)
+        end
+    end
+
+    for k,v in pairs(element.tweens) do --go through all element tweens
+        for kk, vv in pairs(tweens) do --check the same type as type
+            if v == vv and #tweens > 1 then --remove any earlier tweens
+                table.remove(tweens,kk)
+                table.remove(element.tweens,k)
+            end
+        end
+    end
+end
+
 local function newTween(start, dest, duration, easing)
     --putting the check here checks everything
-    if not easingModule then error('Easing module not found! It can be downloaded at https://github.com/EmmanuelOga/easing/blob/master/lib/easing.lua') end
+    if not easingModule then error('Easing module not found! There is a link to it at the top of this script.') end
     local tween = {
         start = start, --starting value
         dest = dest, --destination
@@ -68,39 +88,39 @@ local function newTween(start, dest, duration, easing)
 
         easingStyle = easing or 'linear',
     }
-    
+    return tween --returns tween table with basic properties
+end
+
+local function tweenTable(element, property, dest, duration, easing)
+    local tween = {
+        indexes = #element[property],
+        property = property,
+        element = element,
+        type = 'table'..property,
+        
+    }
+    --make tweens for each index
+    for k,v in pairs(element[property]) do
+        tween['t'..k] = newTween(element[property][k], dest[k], duration, easing)
+    end
+
+    table.insert(element.tweens, tween)
+    checkDupeTweens(element,tween.type)
 
     return tween
 end
 
-local function tweenSize(element, size, duration, easing)
-    local id = #tweens+1
+local function tweenNum(element, property, dest, duration, easing)
     local tween = {
-        xs = newTween(element.size[1],size[1],duration, easing),
-        xp = newTween(element.size[2],size[2],duration, easing),
-        ys = newTween(element.size[3],size[3],duration, easing),
-        yp = newTween(element.size[4],size[4],duration, easing),
+        tween = newTween(element[property],dest,duration, easing),
+        property = property,
         element = element,
-        type = 'size'
+        type = 'num'
     }
-    tweens[id] = tween
-
-    return tweens[id]
-end
-
-local function tweenPos(element, pos, duration, easing)
-    local id = #tweens+1
-    local tween = {
-        xs = newTween(element.pos[1],pos[1],duration, easing),
-        xp = newTween(element.pos[2],pos[2],duration, easing),
-        ys = newTween(element.pos[3],pos[3],duration, easing),
-        yp = newTween(element.pos[4],pos[4],duration, easing),
-        element = element,
-        type = 'pos'
-    }
-    tweens[id] = tween
-
-    return tweens[id]
+    table.insert(element.tweens, tween)
+    checkDupeTweens(element,tween.type)
+    assert(type(element[property]) == 'number', 'Property must be a string with the same name as a numbered property')
+    return nil
 end
 
 local function updateTween(tween) --updates delta and sends variables
@@ -108,8 +128,9 @@ local function updateTween(tween) --updates delta and sends variables
     return tween.elapsed, tween.start, tween.dest-tween.start, tween.duration
 end
 
+--retrieve all of the equasions from the module, you could add more to it if you wanted to
 if easingModule then
-    for name, equasion in pairs(easingModule) do --retrieve all of the equasions from the module
+    for name, equasion in pairs(easingModule) do 
         tweenFunctions[name] = function(tween)
             local t,b,c,d = updateTween(tween)
 
@@ -121,6 +142,24 @@ end
 
 
 --/Base Element/----------------------------------------------------------------
+local function getChildren()
+    local children = {}
+    
+    for _,v in pairs(elements) do
+        if v.parent == element then
+            table.insert(children, v)
+        end
+    end
+
+    return children
+end
+
+local function getDescendants()
+    local descendants = {}
+
+    return descendants
+end
+
 local function newElement(size, pos, parent, zindex)
     local element = {
         size = size or {0,100,0,100}, --UDim2 sizes
@@ -135,6 +174,7 @@ local function newElement(size, pos, parent, zindex)
         axis = 'XY', --XX, YY, XY
 
         visible = true,
+        childrenVisible = false, --when set to false, all children are hidden, doesnt change their "visible" value
         backgroundColor = {100,100,100},
 
         radius = 0, --UDim2
@@ -143,29 +183,26 @@ local function newElement(size, pos, parent, zindex)
         type = 'base', --modifying this has no effect but it doesnt benefit anything
         parent = parent or nil,
 
-        getChildren = function(element)
-            local children = {}
-            
-            for _,v in pairs(elements) do
-                if v.parent == element then
-                    table.insert(children, v)
-                end
-            end
 
-            return children
+        getChildren = getChildren,
+        getDescendants = getDescendants,
+
+        --assign tween functions to element
+        tween = function(element, property, dest, duration, easing)
+            --all encompassing tween function with same argument layouts
+            if type(element[property]) == 'number' then
+                tweenNum(element, property, dest, duration, easing)
+            elseif type(element[property]) == 'table' then
+                tweenTable(element, property, dest, duration, easing)
+            else
+                error('Unsupported tween type; ')
+            end
         end,
 
-        tweenSize = tweenSize,
-        tweenPos = tweenPos,
+        tweens = {} --all active tweens applied to element
     }
     
     return element
-end
-
-local function newEffect(effect, zindex)
-    effects[zindex] = moonshine.chain(moonshine.effects[effect])
-
-    return effects[zindex]
 end
 
 
@@ -194,23 +231,46 @@ Thicc2.text = function(size, pos, parent, zindex)
     element.type = 'text'
     element.text = 'Hello world!'
     element.font = love.graphics.newFont(15)
+
+    --alignment only works with textWrap on
     element.horizontalAlign = 'center' --left, right, center
     element.verticalAlign = 'center' --top, bottom, center
     element.wrapText = false
+
     element.textTransparency = 0
     element.textColor = {255,255,255}
+
 
     table.insert(elements, element)
     return element
 end
 
+Thicc2.button = function(size, pos, parent, zindex)
+    local element = Thicc2.text(size,pos,parent,zindex)
+    element.type = 'button'
+    element.clickColor = {150,150,150}
+    element.hoverColor = {125,125,125}
+    
+    --these do nothing when nil they're just a reminder
+    element.hover = nil
+    element.leave = nil
+
+    element.mouse1Down = nil
+    element.mouse2Down = nil
+    element.mouse3Down = nil
+
+    element.mouse1Click = nil
+    element.mouse2Click = nil
+    element.mouse3Click = nil
+
+    return element
+end
 
 --/Draw/------------------------------------------------------------------------
-local function draw(element)
-    local maxWidth, maxHeight = love.graphics.getDimensions()
+local function draw(element, maxWidth, maxHeight, mouseX, mouseY, mouse1Down)
     local width, height = udim2(element,element.size[1],element.size[2],element.size[3],element.size[4],'size')
     local x, y = udim2(element,element.pos[1],element.pos[2],element.pos[3],element.pos[4])
-    
+
     --account for parent position, this will look bad for 1 frame if things are not zindexed properly :(
     if element.parent and element.parent.absoluteSize then x = x+element.parent.absolutePos[1] y = y+element.parent.absolutePos[2] end
     element.absoluteSize = {width,height} --set actual pixel size for children
@@ -220,7 +280,19 @@ local function draw(element)
     if not element.visible then return end
     love.graphics.setColor(element.backgroundColor[1]/255, element.backgroundColor[2]/255, element.backgroundColor[3]/255, math.abs(element.transparency-1))
 
-    love.graphics.rectangle('fill', x, y, width, height, element.radius,element.radius, element.radiusSegments)
+    --detect hover/clicking
+    if element.hoverColor then
+        if mouseX >= element.absolutePos[1] and mouseX <= element.absolutePos[1]+element.absoluteSize[1] and mouseY >= element.absolutePos[2] and mouseY <= element.absolutePos[2]+element.absoluteSize[2] then
+            if mouse1Down then
+                love.graphics.setColor(element.clickColor[1]/255, element.clickColor[2]/255, element.clickColor[3]/255, math.abs(element.transparency-1))
+            else
+                love.graphics.setColor(element.hoverColor[1]/255, element.hoverColor[2]/255, element.hoverColor[3]/255, math.abs(element.transparency-1))
+            end
+        end
+    end
+    
+    --use math.floor to prevent blurriness
+    love.graphics.rectangle('fill', math.floor(x), math.floor(y), math.floor(width), math.floor(height), element.radius,element.radius, element.radiusSegments)
 
     if element.image then
         --images dont support rounded corners :(
@@ -228,41 +300,29 @@ local function draw(element)
         love.graphics.setColor(element.imageColor[1]/255, element.imageColor[2]/255, element.imageColor[3]/255, math.abs(element.imageTransparency-1))
         love.graphics.draw(element.image, x, y, 0, width/imgWidth, height/imgHeight)-- ox, oy, kx, ky )
     elseif element.text then
-        local text = love.graphics.newText(element.font, element.text)
-        local textWidth, textHeight = text:getDimensions()
-        local textX, textY = 0
-        
-        --horizontal
-        if element.horizontalAlign:lower() == 'left' then
-            textX = 0
-        elseif element.horizontalAlign:lower() == 'right' then
-            textX = width-textWidth
-        else
-            textX = (width/2)-textWidth/2 --roblox math gang
-        end
-
-        --vertical
-        if element.verticalAlign:lower() == 'top' then
-            textY = 0
-        elseif element.verticalAlign:lower() == 'bottom' then
-            textY = height-textHeight
-        else
-            textY = (height/2)-textHeight/2 --width is 1, so you subtract your number/2
-        end
+        local wrap = 99999999 --until a screen has this many X pixels it wont be a problem
+        local textY = element.absolutePos[2]
+        local text = love.graphics.newText(element.font, element.text) --printf is faster but doesnt have Y aligment :(
 
         if element.wrapText then
-            local width, wrappedText = element.font:getWrap(element.text, element.absoluteSize[1]-textX)
+            wrap = element.absoluteSize[1]
+        end
 
-            --make wrapped text
-            local newTextString = ''
-            for _,v in pairs(wrappedText) do
-                newTextString = newTextString..v..'\n'
-            end
-            text:set(newTextString)
+        --apply wrap and align
+        text:setf(element.text, wrap, element.horizontalAlign)
+
+        --set vertical alignment, no top bc it is default
+        if element.verticalAlign == 'center' then
+            textY = (height/2)-text:getHeight()/2
+        elseif element.verticalAlign == 'bottom' then
+            textY = height-text:getHeight()
         end
 
         love.graphics.setColor(element.textColor[1]/255, element.textColor[2]/255, element.textColor[3]/255, math.abs(element.textTransparency-1))
-        love.graphics.draw(text, x+textX, y+textY)
+        love.graphics.setFont(element.font)
+        
+        --keep text from getting blurry
+        love.graphics.draw(text, math.floor(x), math.floor(y+textY))
     end
 end
 
@@ -280,45 +340,51 @@ Thicc2.draw = function()
 		end
 	end
 
+    --sort layers into iterable zindex layers
     table.sort(layerNum)
 
-
-    --render elements
+    --render elements by layer
     for _,layer in pairs(layerNum) do
         for _,element in pairs(layers[layer]) do
-            draw(element)
+            local maxWidth, maxHeight = love.graphics.getDimensions()
+            local mouseX, mouseY = love.mouse.getPosition()
+            draw(element, maxWidth, maxHeight, mouseX, mouseY, love.mouse.isDown(1))
         end        
     end
 
-    
+    --reset color
+    love.graphics.setColor(1,1,1)
 end
+
 
 Thicc2.update = function(delta) --required for tweens
     dt = delta
-    for k,v in pairs(tweens) do
-        if v.type == 'size' then
-            if not tweenFunctions[v.xs.easingStyle] then error('Unknown easing style "'..v.xs.easingStyle..'"') end
-            v.element.size[1] = tweenFunctions[v.xs.easingStyle](v.xs)
-            v.element.size[2] = tweenFunctions[v.xp.easingStyle](v.xp)
-            v.element.size[3] = tweenFunctions[v.xs.easingStyle](v.ys)
-            v.element.size[4] = tweenFunctions[v.yp.easingStyle](v.yp)
-            
-            if v.xs.elapsed == v.xs.duration then
-                table.remove(tweens, k)
+    --update all tweens
+    for _, element in pairs(elements) do
+        if #element.tweens >  0 then --if there are tweens
+            for k, tween in pairs(element.tweens) do
+                --update induvidual tween types    
+                if string.sub(tween.type,1,5) == 'table' then
+                    if not tweenFunctions[tween.t1.easingStyle] then error('Unknown easing style "'..tween.t1.easingStyle..'"') end
+                    
+                    for i = 1, tween.indexes do
+                        tween.element[tween.property][i] = tweenFunctions[tween['t'..tostring(i)].easingStyle](tween['t'..tostring(i)])
+                        --v.element.color[1] = tweenFunctions[v.r.easingStyle](v.r)
+                    end
+
+                    if tween.t1.elapsed == tween.t1.duration then
+                        table.remove(element.tweens, k)
+                    end
+                elseif tween.type == 'num' then
+                    if not tweenFunctions[tween.tween.easingStyle] then error('Unknown easing style "'..tween.xs.easingStyle..'"') end
+                    tween.element[tween.property] = tweenFunctions[tween.tween.easingStyle](tween.tween)
+
+                    if tween.tween.elapsed == tween.tween.duration then
+                        table.remove(element.tweens, k)
+                    end
+                end
             end
-        elseif v.type == 'pos' then
-            if not tweenFunctions[v.xs.easingStyle] then error('Unknown easing style "'..v.xs.easingStyle..'"') end
-            v.element.pos[1] = tweenFunctions[v.xs.easingStyle](v.xs)
-            v.element.pos[2] = tweenFunctions[v.xp.easingStyle](v.xp)
-            v.element.pos[3] = tweenFunctions[v.xs.easingStyle](v.ys)
-            v.element.pos[4] = tweenFunctions[v.yp.easingStyle](v.yp)
-            
-            if v.xs.elapsed == v.xs.duration then
-                table.remove(tweens, k)
-            end
-        end        
-        
-        
+        end
     end
 end
 
