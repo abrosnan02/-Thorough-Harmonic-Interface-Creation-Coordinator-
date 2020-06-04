909ac67cc44f80b276cba0d41114c810ccfa60da
Walnut <44645606+EWalnut@users.noreply.github.com>
2020-06-04 13:48:54 -0400

    Added gradients

--- thicc2.lua
+++ thicc2.lua
@@ -58,6 +58,32 @@ local function udim1(xs, xp, height)
 end
 
 
+--/Gradient/--------------------------------------------------------------------
+Thicc2.gradient = function(colors) --gradient({color1, color2, color3, ..., direction = 'horizontal' or 'vertical'}), taken from Love2D wiki
+    local direction = colors.direction or "horizontal"
+    if direction == "vertical" then
+        direction = true
+    elseif direction == "horizontal" then
+        direction = false
+    else
+        error("Invalid direction '" .. tostring(direction) .. "' for gradient.  Horizontal or vertical expected.")
+    end
+    local result = love.image.newImageData(direction and 1 or #colors, direction and #colors or 1)
+    for i, color in ipairs(colors) do
+        local x, y
+        if direction then
+            x, y = 0, i - 1
+        else
+            x, y = i - 1, 0
+        end
+        result:setPixel(x, y, color[1]/255, color[2]/255, color[3]/255, color[4] or 255)
+    end
+    result = love.graphics.newImage(result)
+    result:setFilter('linear', 'linear')
+    return result --gradient image
+end
+
+
 --/Tweens/----------------------------------------------------------------------
 local function checkDupeTweens(element,type)
     local tweens = {}
